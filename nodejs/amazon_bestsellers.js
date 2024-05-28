78ef5393d6d003ee44a4910406c62f9656d1959b
Žilvinas <159907476+ZilvinasSP@users.noreply.github.com>
2024-05-28 17:08:51 +0300

    Add files via upload

---
+++ nodejs/amazon_bestsellers.js
@@ -0,0 +1,26 @@
+async function fetchAmazonData() {
+    try {
+        const response = await fetch("https://scraper-api.smartproxy.com/v2/scrape", {
+            method: "POST",
+            body: JSON.stringify({
+                "target": "amazon_bestsellers",
+                "query": "sports-collectibles",
+                "page_from": "1",
+                "num_pages": "10",
+                "parse": true,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Basic [your basic authentication token]"
+            }
+        });
+
+        const data = await response.json();
+        console.log(JSON.stringify(data));
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+fetchAmazonData();
