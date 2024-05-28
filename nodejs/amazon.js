8d40eaf4f0636566ee098abdc1e1a3ed4bc5aaa0
Žilvinas <159907476+ZilvinasSP@users.noreply.github.com>
2024-05-28 17:04:10 +0300

    Add files via upload

---
+++ nodejs/amazon.js
@@ -0,0 +1,24 @@
+async function fetchAmazonData() {
+    try {
+        const response = await fetch("https://scraper-api.smartproxy.com/v2/scrape", {
+            method: "POST",
+            body: JSON.stringify({
+                "target": "amazon",
+                "url": "https://www.amazon.com/dp/B08JD3Z1BP/",
+                "parse": true,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Basic [your basic authentication token]"
+            }
+        });
+
+        const data = await response.json();
+        console.log(JSON.stringify(data));
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+fetchAmazonData();
