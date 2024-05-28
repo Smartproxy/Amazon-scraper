5fe291c8794ddbce6c108c4af80afe333eb2b0b1
Žilvinas <159907476+ZilvinasSP@users.noreply.github.com>
2024-05-28 11:29:03 +0300

    Add files via upload

---
+++ amazon.py
@@ -0,0 +1,19 @@
+import requests
+  
+url = "https://scraper-api.smartproxy.com/v2/scrape"
+  
+payload = {
+      "target": "amazon",
+      "url": "https://www.amazon.com/dp/B08JD3Z1BP/",
+      "parse": True,
+}
+  
+headers = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "authorization": "Your basic authentication token"
+}
+  
+response = requests.post(url, json=payload, headers=headers)
+  
+print(response.text)
