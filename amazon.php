8620001263f2ae563cbae37438bed1f41eeaee73
Žilvinas <159907476+ZilvinasSP@users.noreply.github.com>
2024-05-29 15:34:10 +0300

    Add files via upload

---
+++ amazon.php
@@ -0,0 +1,29 @@
+<?php
+$url = "https://scraper-api.smartproxy.com/v2/scrape";
+
+$payload = json_encode([
+    "target" => "amazon",
+    "url" => "https://www.amazon.com/dp/B08JD3Z1BP/",
+    "parse" => true,
+]);
+
+$headers = [
+    "Accept: application/json",
+    "Content-Type: application/json",
+    "Authorization: Basic [your basic authentication token]"
+];
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$response = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
+
+echo $response;
+?>
