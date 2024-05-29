dff42b6c1f22e899c98d9f322d602c634d0e98b2
Žilvinas <159907476+ZilvinasSP@users.noreply.github.com>
2024-05-29 15:34:36 +0300

    Add files via upload

---
+++ php/amazon_bestsellers.php
@@ -0,0 +1,31 @@
+<?php
+$url = "https://scraper-api.smartproxy.com/v2/scrape";
+
+$payload = json_encode([
+    "target" => "amazon_bestsellers",
+    "query" => "sports-collectibles",
+    "page_from" => "1",
+    "num_pages" => "10",
+    "parse" => true,
+]);
+
+$headers = [
+    "Accept: application/json",
+    "Content-Type: application/json",
+    "Authorization: Basic [your basic authentication token]"
+];
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$response = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
+
+echo $response;
+?>
---
+++ php/amazon_questions.php
@@ -0,0 +1,29 @@
+<?php
+$url = "https://scraper-api.smartproxy.com/v2/scrape";
+
+$payload = json_encode([
+    "target" => "amazon_questions",
+    "query" => "B09H74FXNW",
+    "parse" => true,
+]);
+
+$headers = [
+    "Accept: application/json",
+    "Content-Type: application/json",
+    "Authorization: Basic [your basic authentication token]"
+];
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$response = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
+
+echo $response;
+?>
